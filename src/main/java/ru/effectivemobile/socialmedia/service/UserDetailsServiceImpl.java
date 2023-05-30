a008eefc172f6a4cca4d45d29a47d9341dd24c9c
”Ramazan” <mansurov.r13@gmail.com>
2023-05-30 18:22:53 +0300

    Add WebSecurityConfig and UserDetails

---
+++ src/main/java/ru/effectivemobile/socialmedia/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package ru.effectivemobile.socialmedia.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.effectivemobile.socialmedia.model.User;
+import ru.effectivemobile.socialmedia.repository.UserRepository;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
