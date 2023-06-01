5352fc2185cd4a053ede12aa635301eba77446ee
”Ramazan” <mansurov.r13@gmail.com>
2023-06-01 18:48:30 +0300

    new repo - InvitationRepository

---
+++ src/main/java/ru/effectivemobile/socialmedia/repository/InvitationRepository.java
@@ -0,0 +1,13 @@
+package ru.effectivemobile.socialmedia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.effectivemobile.socialmedia.model.Invitation;
+import ru.effectivemobile.socialmedia.model.User;
+
+import java.util.List;
+
+@Repository
+public interface InvitationRepository extends JpaRepository<Invitation, Long> {
+    List<Invitation> getInvitationByRecipientOrderByIdDesc(User recipient);
+}
