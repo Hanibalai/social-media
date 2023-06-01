990993a0380ee7fcece4c29eeeb6f55faec3e7d8
”Ramazan” <mansurov.r13@gmail.com>
2023-06-01 11:39:56 +0300

    added new methods

--- src/main/java/ru/effectivemobile/socialmedia/controller/UserController.java
+++ src/main/java/ru/effectivemobile/socialmedia/controller/UserController.java
@@ -1,42 +1,54 @@
 package ru.effectivemobile.socialmedia.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import ru.effectivemobile.socialmedia.dto.PostDto;
+import ru.effectivemobile.socialmedia.dto.response.MessageResponse;
 import ru.effectivemobile.socialmedia.model.Post;
-import ru.effectivemobile.socialmedia.model.User;
 import ru.effectivemobile.socialmedia.repository.PostRepository;
 import ru.effectivemobile.socialmedia.repository.UserRepository;
 import ru.effectivemobile.socialmedia.service.PostService;
 import ru.effectivemobile.socialmedia.service.UserService;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/auth/user/{id}")
+@PreAuthorize("hasRole('USER')")
+@RequestMapping("api/user")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@AllArgsConstructor
 public class UserController {
-    @Autowired
-    UserService userService;
-    @Autowired
-    PostService postService;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    PostRepository postRepository;
+    private UserService userService;
+    private PostService postService;
+    private UserRepository userRepository;
+    private PostRepository postRepository;
 
-
-    @GetMapping()
-    public ResponseEntity<List<Post>> myPage(@PathVariable("id") long id) {
-        List<Post> userPosts = postService.getPostsOfUser(userRepository.findUserById(id));
+    @GetMapping("/{username}")
+    public ResponseEntity<List<PostDto>> myPage(@PathVariable("username") String username) {
+        List<PostDto> userPosts = postService.getPostsOfUser(username);
         return ResponseEntity.ok(userPosts);
     }
 
-    @PostMapping("/addpost")
-    public ResponseEntity<?> addPost(@PathVariable("id") long id, @RequestBody Post post) {
-        User user = userRepository.findUserById(id);
-        postService.savePost(user, post.getHeader(), post.getText());
-        return ResponseEntity.created(URI.create("/user/id")).body(post);
+    @PostMapping("/{username}/addpost")
+    public ResponseEntity<?> addPost(@PathVariable("username") String username, @RequestBody Post post) {
+        PostDto createdPost = postService.savePost(username, post);
+        return ResponseEntity.ok(createdPost);
     }
 
+    @GetMapping("/{username}/deletepost/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable("username") String username,
+                                        @PathVariable("id") long postId) {
+        if (postService.deletePost(username, postId)) {
+            return ResponseEntity.ok(new MessageResponse("Post was successfully deleted"));
+        }
+        return ResponseEntity.badRequest().body(new MessageResponse("There is some error in request"));
+    }
+
+    @GetMapping("/visit/{username}")
+    public ResponseEntity<List<PostDto>> visitPage(@PathVariable("username") String visitedUsername) {
+        List<PostDto> visitedUserPosts = postService.getPostsOfUser(visitedUsername);
+        return ResponseEntity.ok(visitedUserPosts);
+    }
 }
