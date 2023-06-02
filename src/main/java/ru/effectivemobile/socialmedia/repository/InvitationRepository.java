5383e0ccb1814c903094676a6254caa09063accc
”Ramazan” <mansurov.r13@gmail.com>
2023-06-02 17:21:29 +0300

    new method added

--- src/main/java/ru/effectivemobile/socialmedia/repository/InvitationRepository.java
+++ src/main/java/ru/effectivemobile/socialmedia/repository/InvitationRepository.java
@@ -6,9 +6,11 @@ import ru.effectivemobile.socialmedia.model.Invitation;
 import ru.effectivemobile.socialmedia.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface InvitationRepository extends JpaRepository<Invitation, Long> {
     List<Invitation> getInvitationByRecipientOrderByIdDesc(User recipient);
     boolean existsBySenderAndRecipient(User sender, User recipient);
+    Optional<Invitation> getInvitationByRecipientAndSender(User recipient, User sender);
 }
