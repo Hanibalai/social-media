e81dab97340f5fd65ba186f597793434a941a53b
”Ramazan” <mansurov.r13@gmail.com>
2023-06-03 17:58:12 +0300

    added get methods

--- src/main/java/ru/effectivemobile/socialmedia/repository/MessageRepository.java
+++ src/main/java/ru/effectivemobile/socialmedia/repository/MessageRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> getMessagesBySenderOrderByIdDesc(User sender);
+    List<Message> getMessagesByRecipientOrderByIdDesc(User recipient);
 }
